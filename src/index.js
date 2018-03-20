eb7bb73485cbf6bb96526f0ca0c0d5f9177e5485
theodore <theodoreme@gmail.com>
2018-03-20 21:01:52 +0200

    add home module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import Root from './components/root'
+import store from './store'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Root store={store}/>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/modules/home/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Home extends Component{
+  render(){
+    return( <div>hacker news here</div>)
+  }
+}
+
+export default Home
